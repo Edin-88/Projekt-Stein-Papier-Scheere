8c53349b7ee021e0df1481c47b53d23467332051
Your Name <selmanovic.edin1@gmail.com>
2023-02-07 09:41:06 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,113 @@
-// Form
-const formEins = document.querySelector("#formEins")
-
-// Auswahl der Runden 
-const dreiRunden = document.querySelector("#dreiRunden");
-const fuenfRunden = document.querySelector("#fuenfRunden");
-const zehnRunden = document.querySelector("#zehnRunden");
-const zwanzigRunden = document.querySelector("zwanzigRunden");
-
-// Gesamter Article Bereich bzw. Radio Button Auswahl Bereich
-let rundenAngabe = document.querySelector("#rundenAngabe");
-
-// Gespielte Runden anzeigen sobald erste Auswahl getroffen
-gespielteRunden = document.querySelector("#gespielteRunden")
-
-// Punkteausgabe Benutzer und PC
-let benutzerGewinnt = document.querySelector("#benutzerGewinnt");
-let pcGewinnt = document.querySelector("#pcGewinnt");
-
-// Erbegnisanzeige 
-let ergebnisAusgabe = document.querySelector("#ergebnisAusgabe");
-
-// Spiel neustarten
-const neustarten = document.querySelector("#neustarten");
-
-// Benutzer Auswahl
-const stein = document.querySelector("#stein");
-const papier = document.querySelector("#papier");
-const scheere = document.querySelector("#scheere");
-
-// Punkte zählen
-let punkteBenutzer = 0
-let punktePC = 0
+const benutzerAuswahlMonitor = document.querySelector("#benutzerAuswahl")
+const pcAuswahlMonitor = document.querySelector("#pcAuswahl")
+const ergebnisAusgabe = document.querySelector("#ergebnisAusgabe")
+const auswahlOptionen = document.querySelectorAll("button")
+const benutzerGewinnt = document.querySelector("#benutzerGewinnt")
+const pcGewinnt = document.querySelector("#pcGewinnt")
+const formular = document.querySelector("#formular")
+const rundenAngabe = document.querySelector("#rundenAngabe")
+const gespielteRunden = document.querySelector("#gespielteRunden")
+const endErgebnis = document.querySelector("#endErgebnis")
+const stein = document.querySelector("#stein")
+const papier = document.querySelector("#papier")
+const scheere = document.querySelector("#scheere")
+let benutzerAuswahl
+let pcAuswahl
+let ergebnis
+let benutzerPunkte = 0
+let pcPunkte = 0
 let zaehleRunden = 0
 
+auswahlOptionen.forEach(auswahlOptionen => auswahlOptionen.addEventListener("click", (e) => {
+    benutzerAuswahl = e.target.id
+    benutzerAuswahlMonitor.innerHTML = benutzerAuswahl
+    pcAuswahlGenerieren()
+    ergebnisHolen()
+    PunkteZaehlen()
+    berechneRunden() 
+    spielBeenden() 
+}))
 
-// Funktionen
+function pcAuswahlGenerieren() {
+    const randomNummer = Math.floor(Math.random() * 3 ) + 1  // oder mal auswahlOptionen.length
+    console.log(randomNummer);
 
-//Rundden Auswahl festlegen
-let rundenAnzahl = () => {
-    if(dreiRunden.checked) {
-        zaehleRunden = 3
-        return
-    } else if (fuenfRunden.checked) {
-        zaehleRunden = 5
-        return
-    } else if (zehnRunden.checked) {
-        zaehleRunden = 10
-        return
-    } else if (zwanzigRunden.checked) {
-        return
+    if (randomNummer === 1) {
+        pcAuswahl = "stein"
+    }
+    if (randomNummer === 1) {
+        pcAuswahl = "papier"
+    }
+
+    if (randomNummer === 3) {
+        pcAuswahl = "scheere"
+    }
+    pcAuswahlMonitor.innerHTML = pcAuswahl
+}
+
+function ergebnisHolen() {
+    if (pcAuswahl ===  benutzerAuswahl) {
+        ergebnis = "Unentschieden"
+    }
+    if (pcAuswahl === "stein" && benutzerAuswahl === "papier") {
+        ergebnis = "Du gewinnst"
+    }
+    if (pcAuswahl === "stein" && benutzerAuswahl === "scheere") {
+        ergebnis = "Du verlierst"
+    }
+    if (pcAuswahl === "papier" && benutzerAuswahl === "scheere") {
+        ergebnis = "Du gewinnst"
+    }
+    if (pcAuswahl === "papier" && benutzerAuswahl === "stein") {
+        ergebnis = "Du verlierst"
+    }
+    if (pcAuswahl === "scheere" && benutzerAuswahl === "stein") {
+        ergebnis = "Du gewinnst"
+    } 
+    if (pcAuswahl === "scheere" && benutzerAuswahl === "papier") {
+        ergebnis = "Du verlierst"
+    } 
+    ergebnisAusgabe.innerHTML = ergebnis
+}
+
+
+function PunkteZaehlen () {
+    if (ergebnis === "Du gewinnst") {
+        benutzerPunkte += 1
+        benutzerGewinnt.innerHTML = benutzerPunkte
+    } else if (ergebnis === "Du verlierst") {
+        pcPunkte += 1
+        pcGewinnt.innerHTML = pcPunkte
     } else {
-        return
+        benutzerPunkte += 0 
+        pcPunkte += 0 
     }
 }
 
-// PC PunkteZahl festlegen
-let pcSpielt = () => {
-    let randomWert = Math.floor(Math.random() * 3)
-    return randomWert
+function berechneRunden() {
+    let rundenAuswahl = formular.runden.value
+    rundenAngabe.style.display = "none"
+    zaehleRunden += 1
+    gespielteRunden.innerHTML = `${zaehleRunden} / ${rundenAuswahl}`
 }
 
 
-let steinAuswahl = () => {
-    let rundenWert = formEins.runden.value
+// Funktioniert noch nicht?
+function spielBeenden() {
+    let rundenAuswahl = formular.runden.value
+    
+    if(zaehleRunden === rundenAuswahl) {
+        auswahlOptionen.disabled = true
+    if (benutzerPunkte > pcPunkte) {
+        endErgebnis.innerHTML = "Benutzer hat gewonnen"
+    } else if(benutzerPunkte < pcPunkte) {
+        endErgebnis.innerHTML = "PC hat gewonnen"
+    } else {
+        endErgebnis.innerHTML = "Unentschieden"
+    }
+   }   
+}
 
-} // Musste die Hälfte vom Code löschen da ich von neu angefangen habe,
-  // mache den Rest am Wochenende fertig!
+
+
+
